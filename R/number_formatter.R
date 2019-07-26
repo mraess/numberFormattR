11f8c9a1bb884d71ab6b469e64a913c4a38ee4de
Matthias Raess <matthiasraess@Matthias-Raess-15Pro-2.local>
2019-07-26 11:20:08 -0400

    - inital package commit

---
+++ R/number_formatter.R
@@ -0,0 +1,62 @@
+
+#' Number formatter function
+#'
+#' This function allows you to add a negative "(-)" prefix as well as number suffixes, such as k for thousands, or M for millions.
+#'
+#' @param number A number you want to format - if it is a vector you'll have to map the function to the vector, e.g.
+#' map(vector, number_formatter, rounding = TRUE, digits = 2, currency = TRUE).
+#' @param rounding determines if number gets rounded. Defaults to FALSE.
+#' @param digits determines the number of decimals for rounding.
+#' @param currency determines if the $ sign is prefixed. Defaults to FALSE.
+
+
+number_formatter <- function(number, rounding=FALSE, digits=ifelse(rounding, NA, 6), currency=FALSE)
+                            {
+                              prefix <- ""
+                              if (number<0) {
+                                prefix <- "-"
+                              }
+
+                              number <- abs(number)
+                              lut <- c(1e-24, 1e-21, 1e-18, 1e-15, 1e-12, 1e-09, 1e-06,
+                                       0.001, 1, 1000, 1e+06, 1e+09, 1e+12, 1e+15, 1e+18, 1e+21,
+                                       1e+24, 1e+27)
+                              pre <- c("y", "z", "a", "f", "p", "n", "u", "m", "", "k",
+                                       "M", "B", "T", "P", "E", "Z", "Y", NA)
+
+                              ix <- findInterval(number, lut)
+                              if (ix>0 && ix<length(lut) && lut[ix]!=1) {
+                                if (rounding==TRUE && !is.numeric(digits)) {
+                                  sistring <- paste(prefix,round(number/lut[ix]), pre[ix], sep = "")
+                                }
+                                else if (rounding == TRUE || is.numeric(digits)) {
+                                  sistring <- paste(prefix,signif(number/lut[ix], digits), pre[ix],sep="")
+                                }
+
+                                else {
+                                  sistring <- paste(prefix,number/lut[ix], pre[ix],sep="")
+                                }
+                              } else {
+                                sistring <- paste(prefix,as.character(round(number, digits = digits)),sep="") # the rounding function here ensures that numbers > 0 and < 1000 get rounded as well
+                              }
+
+                              if(currency == TRUE && str_detect(sistring, pattern = "[a-zA-Z]")){
+
+                                sistring <- str_replace_all(string = sistring, pattern = "(-?)([0-9]*\\.?[0-9]*[a-zA-Z])", replacement = "\\1$\\2")
+                              }
+
+                              else if(currency == TRUE && !str_detect(sistring, pattern = "[a-zA-Z]")){ ## the nesting is the problem here
+
+                                sistring <-     str_replace_all(string = sistring, pattern = "(-?)([0-9]*\\.?[0-9]*)", replacement = "\\1$\\2") %>%
+                                  str_replace(pattern = "(\\$)([0-9]*\\.?[0-9]*)(\\$)", replacement = "\\1\\2")
+
+
+                              }
+
+                              else {
+
+                                sistring <- sistring
+                              }
+
+                              return(sistring)
+                            }
---
+++ R/suffix_formatter.R
@@ -0,0 +1,33 @@
+
+#' Number formatter function
+#'
+#' This function allows you to add number suffixes, such as k for thousands,
+#' or M for millions to ggplots. It does NOT return anything when called by itself. Example scale_y_continuous(labels = suffix_formatter_0)
+#'
+#' @param x A vector of numbers representing an axis in your ggplot.
+#' @param digits Defaults to NULL. Here it set to zero for use in plots.
+
+
+suffix_formatter_0 <- function(x, digits = NULL)
+              {
+                intl <- c(1e3, 1e6, 1e9, 1e12);
+                suffixes <- c('k', 'M', 'B', 'T');
+
+                i <- findInterval(x, intl);
+
+                result <- character(length(x));
+
+                # Note: for ggplot2 the last label element of x is NA, so we need to handle it
+                ind_format <- !is.na(x) & i > 0;
+
+                # Format only the elements that need to be formatted
+                # with suffixes and possible rounding
+                result[ind_format] <- paste0(
+                  formatC(x[ind_format]/intl[i[ind_format]], format = "f", digits = 0)
+                  ,suffixes[i[ind_format]]
+                );
+                # And leave the rest with no changes
+                result[!ind_format] <- as.character(x[!ind_format]);
+
+                return(invisible(result));
+              }
